84dcaeeeb848ee5040c13a29695fde0441c13c7f
katspaugh <ivan.kalyaev@gmail.com>
2021-07-14 17:54:58 +0200

    Remove dist and build with a delay

--- dist/endpoint.d.ts
+++
@@ -1,2 +0,0 @@
-import { paths } from './types/gateway';
-export declare function callEndpoint<T extends keyof paths>(network: string, path: T, parameters?: paths[T]['get']['parameters'], rawUrl?: string): Promise<paths[T]['get']['responses'][200]['schema']>;
--- dist/utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type Params = {
-    [key: string]: string | number | boolean | null;
-};
-export declare function insertParams(template: string, params?: Params): string;
-export declare function stringifyQuery(query?: Params): string;
-export declare function fetchJson<T>(url: string): Promise<T>;

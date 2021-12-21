9009ff5e03559503dd11cfb073137d6fb76c0738
katspaugh <381895+katspaugh@users.noreply.github.com>
2021-12-21 10:54:28 +0100

    Unskip test

--- e2e/propose-transaction.test.ts
+++ e2e/propose-transaction.test.ts
@@ -3,7 +3,7 @@ import config from './config'
 
 describe('proposeTransaction tests', () => {
   // Skipping this test, see https://github.com/gnosis/safe-client-gateway/issues/745
-  it.skip('should propose a transaction and fail', async () => {
+  it('should propose a transaction and fail', async () => {
     const req = proposeTransaction(config.baseUrl, '4', '0x4f9BD57BCC68Bf7770429F137922B3afD23d83E7', {
       to: '0x49d4450977E2c95362C13D3a31a09311E0Ea26A6',
       value: '0',
@@ -20,7 +20,7 @@ describe('proposeTransaction tests', () => {
       origin: null,
     })
     await expect(req).rejects.toThrow(
-      '1337: {"nonFieldErrors":["Tx with safe-tx-hash=0x98798b6d9400b25397e85eb79c444a06f93d153555c1d7fd026176f02055a824 for safe=0x4f9BD57BCC68Bf7770429F137922B3afD23d83E7 was already executed in tx-hash=0x1baa941b8696ff3b0a8831f11da243a02f028122b6298ee61e3a1c0d5eeb171a"]}',
+      '1337: {\"nonFieldErrors\":[\"Contract-transaction-hash=0xdf2c4db07ee705e94135d92f835cc241b1d4c20e10d481a4e445265851448cb3 does not match provided contract-tx-hash=0x98798b6d9400b25397e85eb79c444a06f93d153555c1d7fd026176f02055a824\"]}',
     )
   })
 })

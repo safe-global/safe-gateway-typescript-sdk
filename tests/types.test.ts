9fdd18e4e8a798cd35dc226af759e13d01145800
iamacook <aaron.cook@gnosis.pm>
2021-12-21 11:05:47 +0100

    test: Dynamic enum export check

--- tests/types.test.ts
+++ tests/types.test.ts
@@ -1,8 +1,41 @@
-import { SafeAppData, MultisigTransactionRequest, BlockExplorerUriTemplate, FiatCurrencies, MasterCopyReponse, DecodedDataParameterValue } from '../src'
+import {
+  SafeAppData,
+  MultisigTransactionRequest,
+  BlockExplorerUriTemplate,
+  FiatCurrencies,
+  MasterCopyReponse,
+  DecodedDataParameterValue,
+} from '../src'
 
 describe('Types are exported from index correctly', () => {
   it('Random type check', () => {
-    const val = {} as unknown as SafeAppData | MultisigTransactionRequest | BlockExplorerUriTemplate | FiatCurrencies | MasterCopyReponse | DecodedDataParameterValue
+    const val = {} as unknown as
+      | SafeAppData
+      | MultisigTransactionRequest
+      | BlockExplorerUriTemplate
+      | FiatCurrencies
+      | MasterCopyReponse
+      | DecodedDataParameterValue
     expect(val).toBe(val)
+  })
+
+  it('Dynamic enum export check', () => {
+    const { default: _ = null, ...rest } = require('../src/index')
+    const exportedNames = Object.keys(rest)
+
+    const fs = require('fs')
+    const path = require('path')
+
+    const typesDir = path.join(__dirname, '..', 'src', 'types')
+
+    const exportedTypeNames = fs
+      .readdirSync(typesDir)
+      .filter((file) => file !== 'api.ts')
+      .flatMap((file) => {
+        const { default: _ = null, ...rest } = require(`../src/types/${file}`)
+        return Object.keys(rest)
+      })
+
+    exportedTypeNames.every((type) => expect(exportedNames.includes(type)).toBe(true))
   })
 })

e8a1681ec9880aa4fa3dbedafc4aca6cd3813a0c
Aaron Cook <aaron.cook@gnosis.pm>
2022-03-09 17:04:43 +0100

    fix: add `setGuard`/`deleteGuard` types (#65)
    * fix: add `setGuard`/`deleteGuard` types
    
    * fix: extend `SettingsInfo`
    
    * fix: bump version

--- src/types/transactions.ts
+++ src/types/transactions.ts
@@ -128,6 +128,15 @@ export type DisableModule = {
   module: AddressEx
 }
 
+export type SetGuard = {
+  type: 'SET_GUARD'
+  guard: AddressEx
+}
+
+export type DeleteGuard = {
+  type: 'DELETE_GUARD'
+}
+
 export type SettingsInfo =
   | SetFallbackHandler
   | AddOwner
@@ -137,6 +146,8 @@ export type SettingsInfo =
   | ChangeImplementation
   | EnableModule
   | DisableModule
+  | SetGuard
+  | DeleteGuard
 
 export type SettingsChange = {
   type: 'SettingsChange'

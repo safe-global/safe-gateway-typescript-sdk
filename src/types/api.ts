49a5b1b7e7d053e054c260153a30e7632ea0da2a
katspaugh <381895+katspaugh@users.noreply.github.com>
2022-07-26 10:49:51 +0200

    Fix: SafeInfo import (#84)

--- src/types/api.ts
+++ src/types/api.ts
@@ -4,7 +4,6 @@ import {
   SafeBalanceResponse,
   SafeCollectibleResponse,
   SafeIncomingTransfersResponse,
-  SafeInfo,
   SafeModuleTransactionsResponse,
   SafeMultisigTransactionsResponse,
 } from './common'
@@ -15,6 +14,7 @@ import {
   SafeTransactionEstimationRequest,
   TransactionListPage,
 } from './transactions'
+import { SafeInfo } from './safe-info'
 import { ChainListResponse, ChainInfo } from './chains'
 import { SafeAppsResponse } from './safe-apps'
 import { DecodedDataRequest, DecodedDataResponse } from './decoded-data'

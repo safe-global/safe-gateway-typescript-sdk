53d9e76169e177053a79cd7caf895a9042a95f86
Usame Algan <usame@safe.global>
2024-02-28 15:25:06 +0100

    chore: Add comment for 204 status

--- src/utils.ts
+++ src/utils.ts
@@ -41,6 +41,7 @@ async function parseResponse<T>(resp: Response): Promise<T> {
   let json
 
   try {
+    // An HTTP 204 - No Content response doesn't contain a body so trying to call .json() on it would throw
     json = resp.status === 204 ? {} : await resp.json()
   } catch {
     if (resp.headers && resp.headers.get('content-length') !== '0') {
